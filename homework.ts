ecb0019b5350198b891b5a8a4235e6977d53997d
viktoriiadron <88160860+viktoriiadron@users.noreply.github.com>
2022-07-25 23:15:00 +0300

    completed

--- homework.ts
+++ homework.ts
@@ -1,1 +1,37 @@
-// hello word
+
+interface User {
+    readonly id: number;
+    name: string;
+    phoneNumber: string;
+    email: string;
+    isAdmin: boolean;
+}
+
+interface Admin extends User {
+    hasPermition: boolean;
+}
+
+interface Customer extends User {
+    birth: Date;
+    hasSubscription: boolean;
+    ammount: number;
+}
+
+interface DanseHall {
+    name: string;
+    prisePerCustomer: number;
+    priceForRent: number;
+    capacity: number;
+    isEmpty: boolean;
+    equipment?: string[];
+}
+
+interface DanceSchool {
+    name: string;
+    address: string;
+    bankAccount: string;
+    isWorking: boolean;
+    danceHalls: DanseHall[];
+}
+
+console.log('hello world')
